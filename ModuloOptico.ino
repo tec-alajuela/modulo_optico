c77c2105a6ee9e21601702890bdd06bf1f50435d
Jordan Andrés Villalobos Campos <jordanandres1@hotmail.com>
2015-07-10 11:09:23 -0600

    Código fuente

---
+++ ModuloOptico.ino
@@ -0,0 +1,43 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+const int buttonPin = 10;
+const int sensorPin = 0;
+int valorEstable;
+int contador = 0;
+
+void setup() {
+  lcd.begin(16, 2);
+  valorEstable = analogRead(sensorPin)-2;
+  reset();
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+  Serial.print("valor estable:");
+  Serial.println(valorEstable);
+}
+
+void reset(){
+  lcd.clear();
+  lcd.print("Marcador:");
+  lcd.setCursor(0,1);
+  contador = 0;
+  lcd.print(contador);
+}
+
+void loop() {
+  int sensorlight;
+  int buttonState;
+  lcd.setCursor(0,1);
+  sensorlight = analogRead(sensorPin);
+  buttonState = digitalRead(buttonPin);
+  Serial.println(sensorlight);
+  if(buttonState == LOW){
+    delay(500);
+    reset();
+  }
+  else if(sensorlight < valorEstable-30){
+    contador++;
+    lcd.print(contador);
+    delay(2000);
+  }
+}
