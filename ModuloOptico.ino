18f4403502a02c6db97e27ad78291612494a5b49
Jordan Andrés Villalobos Campos <jordanandres1@hotmail.com>
2015-07-16 15:16:13 -0600

    Actualización del código fuente y del modelo de arduino

--- ModuloOptico.ino
+++ ModuloOptico.ino
@@ -1,7 +1,8 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12,11,5,4,3,2);
-const int buttonPin = 10;
+const int buttonPin1 = 10;
+const int buttonPin2 = 13;
 const int sensorPin = 0;
 int valorEstable;
 int contador = 0;
@@ -10,7 +11,8 @@ void setup() {
   lcd.begin(16, 2);
   valorEstable = analogRead(sensorPin)-2;
   reset();
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
   Serial.begin(9600);
   Serial.print("valor estable:");
   Serial.println(valorEstable);
@@ -26,14 +28,23 @@ void reset(){
 
 void loop() {
   int sensorlight;
-  int buttonState;
+  int buttonState1;
+  int buttonState2;
   lcd.setCursor(0,1);
   sensorlight = analogRead(sensorPin);
-  buttonState = digitalRead(buttonPin);
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2 = digitalRead(buttonPin2);
   Serial.println(sensorlight);
-  if(buttonState == LOW){
+  if(buttonState1 == LOW){
     delay(500);
     reset();
+  }
+  else if(buttonState2 == LOW){
+    if(contador > 0){
+      contador--;
+    }
+    lcd.print(contador);
+    delay(500);
   }
   else if(sensorlight < valorEstable-30){
     contador++;
@@ -41,3 +52,4 @@ void loop() {
     delay(2000);
   }
 }
+
